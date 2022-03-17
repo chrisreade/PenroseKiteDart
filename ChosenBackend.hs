8916604182679e6c5669b21af94a38638c6a4b88
Chris Reade <chrisreade@mac.com>
2022-03-17 13:13:17 +0000

    Reorganised Files
    src folder

--- ChosenBackend.hs
+++
@@ -1,3 +0,0 @@
-module ChosenBackend (B, mainWith) where
-import Diagrams.Backend.SVG.CmdLine
--- import Diagrams.Backend.Postscript.CmdLine
---
+++ app/Main.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE TypeFamilies              #-}
+    
+import Diagrams.Prelude
+import ChosenBackend (B, mainWith)
+
+import TileFigExamples
+-- import DecompExplain (used for the single figure decompExplainFig)
+
+{- e.g
+markedTiles, componentsFig, componentsFig2, fourDecomps, fiveInflate, fiveAlternatives, 
+sun6Fig, sun5Over6Fig, filledSun6, filledSun6' , experimentFig, 
+-}
+{- and colour examples: swatches and samples
+e.g. showSample (darkcyan, darkblue, wheat)
+-}
+
+import GraphFigExamples
+
+-- imported for testing / debugging only
+import TileLib
+import HalfTile
+import Tgraphs
+import GraphConvert
+
+                
+fig::Diagram B
+fig = twoChoicesFig
+
+
+main = mainWith fig
